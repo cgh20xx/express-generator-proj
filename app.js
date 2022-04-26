46b6f835d15a858078b1e2d110fdd6998961f1ee
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2022-04-26 21:16:23 +0800

    add mongoose cors

--- app.js
+++ app.js
@@ -2,15 +2,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
+mongoose
+  .connect('mongodb://localhost:27017/test')
+  .then(() => {
+    console.log('mongodb 連接成功');
+  })
+  .catch((error) => console.log(error));
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false })); // 這行會 parse body
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
